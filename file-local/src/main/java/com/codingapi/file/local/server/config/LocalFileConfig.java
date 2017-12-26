fd5a4a11c5598cba3c49ca4658721dc8e0df2c3a
wangliang <1991wangliang@gmail.com>
2017-12-26 22:25:35 +0800

    codingapi

---
+++ file-local/src/main/java/com/codingapi/file/local/server/config/LocalFileConfig.java
@@ -0,0 +1,20 @@
+package com.codingapi.file.local.server.config;
+
+/**
+ * create by lorne on 2017/12/26
+ */
+public class LocalFileConfig {
+
+    /**
+     * 文件存储地址
+     */
+    private String fileServerPath;
+
+    public String getFileServerPath() {
+        return fileServerPath;
+    }
+
+    public void setFileServerPath(String fileServerPath) {
+        this.fileServerPath = fileServerPath;
+    }
+}
